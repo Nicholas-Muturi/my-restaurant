c83a7c1b3ad06b46c1d6d810f725809011dd9805
Nicholas Muturi <nicholasmuturi1@gmail.com>
2019-11-05 16:54:15 +0300

    [test] created test to simulate user interaction with simple app

---
+++ app/src/androidTest/java/m/nicholas/myrestaurant/MainActivityInstrumentationTest.java
@@ -0,0 +1,42 @@
+package m.nicholas.myrestaurant;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText(){
+        onView(withId(R.id.locationEditText)).perform(typeText("Portland")).check(matches(withText("Portland")));
+    }
+
+    @Test
+    public void locationIsSentToRestaurantActivity(){
+        String location = "Portland";
+        onView(withId(R.id.locationEditText)).perform(typeText(location)).perform(closeSoftKeyboard());
+        try {                             // the sleep method requires to be checked and handled so we use try block
+            Thread.sleep(250);
+        } catch (InterruptedException e){
+            System.out.println("got interrupted!");
+        }
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches(withText("Here are all the restaurants near: " + location)));
+    }
+}
