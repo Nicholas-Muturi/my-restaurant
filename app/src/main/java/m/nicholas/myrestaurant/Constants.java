a07492e1bbbe6fbbd45cd87755ab513fe665cf89
Nicholas Muturi <nicholasmuturi1@gmail.com>
2019-11-12 12:51:03 +0300

    [chore] include image and constants class to git

---
+++ app/src/main/java/m/nicholas/myrestaurant/Constants.java
@@ -0,0 +1,6 @@
+package m.nicholas.myrestaurant;
+
+public class Constants {
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+}
